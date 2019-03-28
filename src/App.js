e6905d82d14f1376a6b7f46e52b5c7c9c2def730
Alain <alain.goldman@gmail.com>
2019-03-28 16:29:29 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <h2> some page here </h2>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +20,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
       </div>
     );
   }
